5457acbf73504e7e491f81d4c96cfcc522c8404e
Konstantin Krupovich <kkrupovich@gmail.com>
2021-08-18 00:36:20 +0300

    #5 Broadcast offers in batches
    20 per batch for now

--- edmprefui/src/app/services/state.service.ts
+++ edmprefui/src/app/services/state.service.ts
@@ -6,7 +6,10 @@ import { StorageService } from './storage.service';
 
 const USER_INFO_KEY = 'USER_INFO';
 const OFFERS_KEY = 'OFFERS';
-
+export interface  IncomingOffer {
+  connectionId: string; // offerer's connection id
+  offer: UserInfo; // offer
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -14,6 +17,8 @@ export class StateService {
 
   private _userInfo$ = new BehaviorSubject<UserInfo>(DEFAULT_USER_INFO);
   private _offers$ = new BehaviorSubject<Array<UserInfo>>([]);
+
+  private _getoffersReset = false;
 
   public userInfo$ = this._userInfo$.asObservable();
   public offers$ = this._offers$.asObservable();
@@ -45,18 +50,42 @@ export class StateService {
   cleanUpInboundOffers() {
     this._offers$.next([]);
   }
-  processInboundOffer(offer: UserInfo) {
-    if (offer.connectionid === "" ||
-      offer.connectionid === this._userInfo$.value.connectionid) return; // skip my own offer broadcast
 
+  //* To process batch of offers we should 1st remove old offers. to
+  //* avoid empty "current offers" list we turn the flag ON when reques sent
+  //* and OFF when batches start to drop into our inbox in processInboundOffersBatch
+  prepareOffersBatchProcessing() {
+    this._getoffersReset = true;
+  }
+
+  processInboundOffersBatch(batch: Array<IncomingOffer>) {
     var offers = this._offers$.value;
-    var idxExisting = offers.findIndex(o => o.connectionid === offer.connectionid);
-    if (idxExisting >= 0) {
-      offers[idxExisting].items = offer.items;
-    } else {
-      offers.push(offer);
+    if (this._getoffersReset) { // flag toggled on getoffers request and resetted upon 1st batch processing
+      offers = [];
+      this._getoffersReset = false;
     }
+    batch.forEach(offer => {
+      this.processOneInboundOffer(offer, offers);
+    });
     this._offers$.next(offers);
+  }
+
+  processInboundOffer(offer: UserInfo) {
+    var offers = this._offers$.value;
+    this.processOneInboundOffer({ connectionId: offer.connectionid??"", offer }, offers);
+    this._offers$.next(offers);
+  }
+
+  processOneInboundOffer(offer: IncomingOffer, offers: Array<UserInfo>) {
+    if (offer.connectionId === "" ||
+      offer.connectionId === this._userInfo$.value.connectionid) return; // skip my own offer broadcast
+
+    var idxExisting = offers.findIndex(o => o.connectionid === offer.connectionId);
+    if (idxExisting >= 0) {
+      offers[idxExisting].items = offer.offer.items;
+    } else {
+      offers.push(offer.offer);
+    }
   }
 
   updateUserTradeItems(items: Array<TradeItem>) {
