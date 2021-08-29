a8c0c4e918419957a7e8aa9de9f63af783464a43
Konstantin Krupovich <kkrupovich@gmail.com>
2021-08-29 05:19:17 +0300

    #14 fix for localisation assets loader
    Default loader path from transloco docs doesn't work for me. The only way to fix is change the path from absolute to relative \O/

--- edmprefui/src/app/transloco/transloco-root.module.ts
+++ edmprefui/src/app/transloco/transloco-root.module.ts
@@ -15,7 +15,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   constructor(private http: HttpClient) {}
 
   getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+    return this.http.get<Translation>(`./assets/i18n/${lang}.json`);
   }
 }
 
@@ -27,6 +27,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
       useValue: translocoConfig({
         availableLangs: ['en', 'es', 'de', 'fr', 'pt', 'ru'],
         defaultLang: 'en',
+        fallbackLang: 'en',
         // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
         prodMode: environment.production,
